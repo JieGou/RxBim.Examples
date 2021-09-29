8672f176183c75cea94349fcaf28fa0214250c59
pavlovek <pavlovek@pik.ru>
2021-09-29 13:23:58 +0300

    add nuke build

---
+++ build/Build.cs
@@ -0,0 +1,14 @@
+using Nuke.Common.Execution;
+using RxBim.Nuke.Revit;
+
+[CheckBuildProjectConfigurations]
+class Build : RevitRxBimBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+}
